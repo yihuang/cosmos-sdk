a75a2c77cb983d1cdce252e622c4eaf06b130efd
Robert Zaremba <robert@zaremba.ch>
2020-09-21 13:29:38 +0200

    x/bank: introduce Handle method for messages

--- x/bank/handler.go
+++ x/bank/handler.go
@@ -1,9 +1,6 @@
 package bank
 
 import (
-	metrics "github.com/armon/go-metrics"
-
-	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/bank/keeper"
@@ -16,79 +13,11 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
-		case *types.MsgSend:
-			return handleMsgSend(ctx, k, msg)
-
-		case *types.MsgMultiSend:
-			return handleMsgMultiSend(ctx, k, msg)
+		case *types.MsgSend, *types.MsgMultiSend:
+			return msg.Handle(ctx, k)
 
 		default:
 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized bank message type: %T", msg)
 		}
 	}
-}
-
-// Handle MsgSend.
-func handleMsgSend(ctx sdk.Context, k keeper.Keeper, msg *types.MsgSend) (*sdk.Result, error) {
-	if err := k.SendEnabledCoins(ctx, msg.Amount...); err != nil {
-		return nil, err
-	}
-
-	if k.BlockedAddr(msg.ToAddress) {
-		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive funds", msg.ToAddress)
-	}
-
-	err := k.SendCoins(ctx, msg.FromAddress, msg.ToAddress, msg.Amount)
-	if err != nil {
-		return nil, err
-	}
-
-	defer func() {
-		for _, a := range msg.Amount {
-			telemetry.SetGaugeWithLabels(
-				[]string{"tx", "msg", "send"},
-				float32(a.Amount.Int64()),
-				[]metrics.Label{telemetry.NewLabel("denom", a.Denom)},
-			)
-		}
-	}()
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-		),
-	)
-
-	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
-}
-
-// Handle MsgMultiSend.
-func handleMsgMultiSend(ctx sdk.Context, k keeper.Keeper, msg *types.MsgMultiSend) (*sdk.Result, error) {
-	// NOTE: totalIn == totalOut should already have been checked
-	for _, in := range msg.Inputs {
-		if err := k.SendEnabledCoins(ctx, in.Coins...); err != nil {
-			return nil, err
-		}
-	}
-
-	for _, out := range msg.Outputs {
-		if k.BlockedAddr(out.Address) {
-			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive transactions", out.Address)
-		}
-	}
-
-	err := k.InputOutputCoins(ctx, msg.Inputs, msg.Outputs)
-	if err != nil {
-		return nil, err
-	}
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-		),
-	)
-
-	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
 }
--- x/bank/types/msgs.go
+++ x/bank/types/msgs.go
@@ -1,8 +1,12 @@
 package types
 
 import (
+	"github.com/armon/go-metrics"
+	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	"github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 // bank message types
@@ -12,6 +16,7 @@ const (
 )
 
 var _ sdk.Msg = &MsgSend{}
+var _ sdk.Msg = &MsgMultiSend{}
 
 // NewMsgSend - construct a msg to send coins from one account to another.
 func NewMsgSend(fromAddr, toAddr sdk.AccAddress, amount sdk.Coins) *MsgSend {
@@ -55,7 +60,39 @@ func (msg MsgSend) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.FromAddress}
 }
 
-var _ sdk.Msg = &MsgMultiSend{}
+func (m *MsgSend) Handle(ctx sdk.Context, k keeper.Keeper) (*sdk.Result, error) {
+	if err := k.SendEnabledCoins(ctx, msg.Amount...); err != nil {
+		return nil, err
+	}
+
+	if k.BlockedAddr(msg.ToAddress) {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive funds", msg.ToAddress)
+	}
+
+	err := k.SendCoins(ctx, msg.FromAddress, msg.ToAddress, msg.Amount)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		for _, a := range msg.Amount {
+			telemetry.SetGaugeWithLabels(
+				[]string{"tx", "msg", "send"},
+				float32(a.Amount.Int64()),
+				[]metrics.Label{telemetry.NewLabel("denom", a.Denom)},
+			)
+		}
+	}()
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+		),
+	)
+
+	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
+}
 
 // NewMsgMultiSend - construct arbitrary multi-in, multi-out send msg.
 func NewMsgMultiSend(in []Input, out []Output) *MsgMultiSend {
@@ -96,6 +133,35 @@ func (msg MsgMultiSend) GetSigners() []sdk.AccAddress {
 	}
 
 	return addrs
+}
+
+// Handle MsgMultiSend.
+func (msg *MsgMultiSend) Handle(ctx sdk.Context, k keeper.Keeper) (*sdk.Result, error) {
+	// NOTE: totalIn == totalOut should already have been checked
+	for _, in := range msg.Inputs {
+		if err := k.SendEnabledCoins(ctx, in.Coins...); err != nil {
+			return nil, err
+		}
+	}
+	for _, out := range msg.Outputs {
+		if k.BlockedAddr(out.Address) {
+			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive transactions", out.Address)
+		}
+	}
+
+	err := k.InputOutputCoins(ctx, msg.Inputs, msg.Outputs)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+		),
+	)
+
+	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
 }
 
 // ValidateBasic - validate transaction input
